a8d3601918fc2dbab3ee69fe54d88a484c19e4aa
PetJs <peterfagoroye76@gmail.com>
2023-09-09 13:07:39 +0100

    restructing app

--- index.js
+++ index.js
@@ -7,16 +7,24 @@ let sum = document.getElementById("sum-el")
 function add(){
     result = parseInt(a.value) + parseInt(b.value)
     sum.textContent += result
+    a.value = ""
+    b.value = ""
 }
 function subtract(){
     result = parseInt(a.value) - parseInt(b.value)
     sum.textContent += result 
+    a.value = ""
+    b.value = ""
 }
 function divide(){
     result = parseInt(a.value) / parseInt(b.value)
     sum.textContent += result
+    a.value = ""
+    b.value = ""
 }
 function multiply(){
     result = parseInt(a.value) * parseInt(b.value)
     sum.textContent += result
+    a.value = ""
+    b.value = ""
 }
